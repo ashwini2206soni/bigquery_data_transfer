4c1807dbb00ca0a6c21cfc0a0923179213087b51
ashwinisoni2206 <ashwini19soni@gmail.com>
2021-02-20 20:55:42 -0500

    updated secret key variables in parameters file

--- ci/task_deploy.sh
+++ ci/task_deploy.sh
@@ -6,14 +6,14 @@ cd terraform
 
 # move concourse parameters into files for terraform consumption
 echo "${GCP_CREDENTIALS}" > credentials.json
-echo "${AWS_AKI}" 
-echo "${AWS_SAK}"
+# echo "${AWS_AKI}" 
+# echo "${AWS_SAK}"
 
 echo "==> Init <=="
 terraform init \
     -input=false \
     -no-color \
-    ./terraform
+    
 
 echo "==> Plan <=="
 terraform plan \
@@ -22,8 +22,8 @@ terraform plan \
     -var-file=terraform/default.tfvars \
     -var "access_key_id=${AWS_AKI}" \
     -var "secret_access_key=${AWS_SAK}" \
-    -no-color \
-    ./terraform
+    -no-color 
+    
 
 echo "==> Apply <=="
 terraform apply \
