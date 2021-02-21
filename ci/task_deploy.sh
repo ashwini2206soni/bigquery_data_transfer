9d68fd0bddb0f18add859368eefc66be7ef00773
ashwinisoni2206 <ashwini19soni@gmail.com>
2021-02-21 02:54:26 -0500

    update task_deploy.sh again again

--- ci/task_deploy.sh
+++ ci/task_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -e
-cd terraform
+
 
 
 # move concourse parameters into files for terraform consumption
@@ -12,24 +12,26 @@ echo "${GCP_CREDENTIALS}" > credentials.json
 echo "==> Init <=="
 terraform init \
     -input=false \
-    -no-color 
+    -no-color \
+    ./terraform
     
 echo `ls `
 echo "==> Plan <=="
 terraform plan \
-    -var-file=./default.tfvars \
+    -var-file=terraform/default.tfvars \
     -input=false \
     -var "access_key_id=${AWS_AKI}" \
     -var "secret_access_key=${AWS_SAK}" \ 
     -no-color \
-    -detailed-exitcode
+    -detailed-exitcode \
+    ./terraform
     
-
 echo "==> Apply <=="
 terraform apply \
-    -var-file=default.tfvars \
+    -var-file=terraform/default.tfvars \
     -var "access_key_id=${AWS_AKI}" \
     -var "secret_access_key=${AWS_SAK}" \ 
     -input=false \
-    -no-color
+    -no-color \
+    ./terraform
 echo "==> Done <=="
